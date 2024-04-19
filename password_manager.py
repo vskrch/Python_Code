bc2224bb0f154e53fe3911fd4651c5fa40a11b63
VS <vskrch@gmail.com>
2024-04-18 21:29:27 -0400

    P4 Password maanger - Files

--- password_manager.py
+++ password_manager.py
@@ -1,24 +1,53 @@
-#Don't save passwords to txt file lol
+
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+        key_file.close()
+
+def load_key():
+    with  open("key.key", "rb") as file:
+        key = file.read()
+        file.close()
+    return key
 master_pwd = input("What is the master password?")
 
-def view():
-    with open('passwords.txt','r') as file:
-        for line in file.readlines():
-            data = line.rstrip()
-        username, password = data.split("|")
-        print("Username:",username,"Password:",password)
 
-        file.close()
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
+
+
+
+
+
+def view():
+    try:
+        with open('passwords.txt', 'r') as file:
+            for line in file.readlines():
+                data = line.rstrip()
+                username, password = data.split("|")
+                print("Username:", username, "Password:", fer.decrypt(password.encode()).decode())
+    except FileNotFoundError:
+        print("Password file not found. Create one by adding passwords.")
+
+
+
 def add():
-    name  =  input("Account Name")
-    pwd  = input("password:")
+    name = input("Account Name")
+    pwd = input("password:")
 
     with open('passwords.txt', 'a') as file:
-        file.write(name+"|"+pwd)
+        file.write(name + "|" + str(fer.encrypt(pwd.encode())))
         file.close()
+
+
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit").lower()
-    if mode =='q':
+    if mode == 'q':
         break
 
     if mode == 'view':
